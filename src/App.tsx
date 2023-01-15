1bd501d291ea674591a0176ccc146cda3a32cd50
shayan vosoughi <shayanwqhw@gmail.com>
2023-01-15 06:32:08 +0330

    feat: :hammer: Build Routes Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
 import Header from './Includes/Header/Header'
 import Footer from './Includes/Footer/Footer'
-import { Routes } from 'react-router-dom'
-import { Route } from 'react-router'
 import Home from './Containers/Home/Home'
+import Shop from './Containers/Shop/Shop'
+import Blog from './Containers/Blog/Blog'
+import Tags from './Containers/Tags/Tags'
+import AboutUs from './Containers/AboutUs/AboutUs'
+import Faqs from './Containers/FAQs/faqs'
+import NotFound from './Components/Errors/NotFound'
+import Dashboard from './Containers/Dashboard/Dashboard'
+import Product from './Containers/Product/Product'
+import { Article } from '@mui/icons-material'
+import Tag from './Containers/Tag/Tag'
 
 const App = () => {
   return (
@@ -10,6 +19,16 @@ const App = () => {
       <Header />
       <Routes>
         <Route element={<Home />} path='/' />
+        <Route element={<Shop />} path="/shop" />
+        <Route element={<Product />} path="/shop/{tag}/{title}" />
+        <Route element={<Blog />} path="/blog" />
+        <Route element={<Article />} path="/blog/{title}" />
+        <Route element={<Tags />} path="/tags" />
+        <Route element={<Tag />} path="/tag/{title}" />
+        <Route element={<AboutUs />} path="/about-us" />
+        <Route element={<Faqs />} path="/FAQs" />
+        <Route element={<Dashboard />} path="/dashboard" />
+        <Route element={<NotFound />} path="*" />
       </Routes>
       <Footer />
     </>
---
+++ src/Containers/FAQs/faqs.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Faqs = () => {
+  return (
+    <div>faqs</div>
+  )
+}
+
+export default Faqs
