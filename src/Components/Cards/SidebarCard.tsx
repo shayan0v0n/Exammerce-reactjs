2eb2ea27083f438aae6e6d449be24cdf45976399
shayan vosoughi <shayanwqhw@gmail.com>
2023-02-06 18:23:10 +0330

    feat: :hammer: Add Sidebar In Homepage

---
+++ src/Components/Cards/SidebarCard.tsx
@@ -0,0 +1,54 @@
+import { Grid, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+
+interface product {
+    title:string
+    author:string
+    img:string
+}
+
+const SidebarCard = () => {
+    const products: product[] = [
+        {
+            title: 'Life Flight',
+            author: 'Misty Figueroa',
+            img: '/assets/imgs/sidebarProducts/1.jpg'
+        },
+        {
+            title: 'Heartland Stars',
+            author: 'Ernesto Wade',
+            img: '/assets/imgs/sidebarProducts/2.jpg'
+        },
+        {
+            title: 'Goodbye Again',
+            author: 'Gilberto Mills',
+            img: '/assets/imgs/sidebarProducts/3.jpg'
+        },
+        {
+            title: 'Annie Leibovitz:',
+            author: 'Rita James',
+            img: '/assets/imgs/sidebarProducts/4.jpg'
+        },
+        {
+            title: 'The Good Egg',
+            author: 'Arthur Gonzalez',
+            img: '/assets/imgs/sidebarProducts/5.jpg'
+        }
+    ]
+
+  return (
+    <Box>
+            { products.map((item:product ,) => (
+                <Box sx={{ margin: '2rem 1rem', display:'flex', justifyContent:'space-start', alignItems:'center' }}>
+                        <img src={item.img} style={{ width: '30%', borderRadius: '10px' }} />
+                        <Box sx={{ margin: '0 1rem' }}>
+                            <Typography sx={{ fontWeight: 'bold' }} variant='h5'>{item.title}</Typography>
+                            <Typography>{item.author}</Typography>
+                        </Box>
+                </Box>
+            )) }
+    </Box>
+  )
+}
+
+export default SidebarCard
--- src/Containers/Home/Home.tsx
+++ src/Containers/Home/Home.tsx
@@ -1,15 +1,22 @@
-import { Grid } from '@mui/material'
+import { Divider, Grid, Typography } from '@mui/material'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
-
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import 'swiper/css/autoplay';
+import { Box } from '@mui/system';
+import SidebarCard from '../../Components/Cards/SidebarCard';
+
+interface sliderItem {
+  img: string
+  tilte: string
+  des: string
+}
 
 const Home = () => {
-  const dataSlider = [
+  const dataSlider:sliderItem[] = [
     {
       img: "/assets/imgs/homeSlider/1.jpg",
       tilte: "1این یک پیام تست است",
@@ -29,11 +36,15 @@ const Home = () => {
 
   return (
     <>
-      <Grid container sx={{ margin: '2rem 0' }}>
-        <Grid xs={4}>
-
+      <Grid container sx={{ margin: '2rem 0' }} justifyContent="space-around">
+        <Grid xs={3} item>
+          <Box sx={{ padding: '1.5rem', margin: '2rem', background: "#D9D9D9", borderRadius: '8px' }}>
+            <Typography variant='h4' fontWeight="bold">Top 10 This Week</Typography>
+              <Divider sx={{ margin: "1rem" }} />
+            <SidebarCard />
+          </Box>
         </Grid>
-        <Grid xs={8}>
+        <Grid xs={8} item>
         <Swiper
           style={{ margin: "2rem", borderRadius: '15px' }}
           spaceBetween={50}
@@ -47,7 +58,7 @@ const Home = () => {
               disableOnInteraction: false
           }}
         >
-          { dataSlider.map((item) => (
+          { dataSlider.map((item:sliderItem) => (
             <SwiperSlide><img src={item.img} width="100%" /></SwiperSlide>
           )) }
         </Swiper>
